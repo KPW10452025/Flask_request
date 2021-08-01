818223e452ddff4aa1af96135b86f81952e393ad
PoWei <10452025@mail.ntou.edu.tw>
2021-08-01 16:13:45 +0900

    request的簡單應用

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route("/query_example")
 def query_example():
-	request
-	return "Todo ..."
+	language01 = request.args.get("language")
+	return "The language is : {}".format(language01)
 
 if __name__ == "__main__":
 	app.run(debug = True, port = 3000)
@@ -14,3 +14,12 @@ if __name__ == "__main__":
 # 要練習的項目為在網址中加入文字如下
 # localhost:3000/query_example?key1=value1&key2=value2
 # 加入這些文字時，必須使用問號當作開頭
+# 這時就能使用 request 達成需求
+# language01 = request.args.get("language")
+# return "The language is : {}".format(language01)
+# 這時再次刷新瀏覽器網頁會顯示，網址為 localhost:3000/query_example
+# 網頁會顯示 The language is : None
+# 將網址 localhost:3000/query_example 後面加問號 ? 後再加上 language=Python
+# localhost:3000/query_example?language=Python
+# 頁面刷新後會得到
+# The language is : Python
