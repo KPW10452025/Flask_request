bd305030bbe8f29948d1517a8bf3fbb54850c8de
PoWei <10452025@mail.ntou.edu.tw>
2021-08-02 00:31:08 +0900

    製作完整表格輸入並提交後頁面互動

--- app.py
+++ app.py
@@ -12,21 +12,26 @@ def query_example():
 	          <p>The framework is : {}<p>
 	          <p>The website is : {}<p>""".format(language01, framework01, website01)
 
-@app.route("/form_example", methods=["POST", "GET"])
+@app.route("/form_example", methods=["POST", "GET"]) # localhost:3000/form_example
 def form_example():
 	if request.method == "POST":
-		return "<p>Submitted Form</P>"
+		language03 = request.form.get("language02")
+		framework03 = request.form["framework02"]
+		return "<p>The language is {}, and the framework is {}.</P>".format(language03, framework03)
 
 	return """<form method="POST"> 
-	<p>Please input Language <input type="text" name="language"></p>
-	<p>Please input Framework <input type="text" name="framework"></p>
+	<p>Please input Language <input type="text" name="language02"></p>
+	<p>Please input Framework <input type="text" name="framework02"></p>
 	<p><input type="submit"></p>
 	</form>"""
 
 if __name__ == "__main__":
 	app.run(debug = True, port = 3000)
 
-# 目前只要點選提交後，頁面都會轉換到 Method Not Allowed
-# 在 route 中添加 methods=["POST", "GET"]
-# 並用 if 判斷 request.method 是否為 POST 是的話頁面顯示 Submitted Form
-# 重新開啟 localhost:3000/form_example 後，現在按下提交會就會出現已設定好的 Submitted Form
+# 現在要讓表格的輸入和 return 後的頁面做互動
+# 順便看看有 .get() 和直接用 [] 的差異
+# 為區別每個 language 和 framework 所以便在後面添加 01, 02, 03
+# 完成後的表格若不填寫點提交後會顯示以下
+# The language is , and the framework is .
+# 填寫 Python 和 Flask 後提交會顯示如下
+# The language is Python, and the framework is Flask.
