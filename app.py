c945c792e2c4240c49d9e46f1774d19ac87b1d3d
PoWei <10452025@mail.ntou.edu.tw>
2021-08-02 08:36:19 +0900

    將 framework02 改為 framework99 來測試 [] 效果

--- app.py
+++ app.py
@@ -20,16 +20,17 @@ def form_example():
 		return "<p>The language is {}, and the framework is {}.</P>".format(language03, framework03)
 
 	return """<form method="POST"> 
-	<p>Please input Language <input type="text" name="language99"></p>
-	<p>Please input Framework <input type="text" name="framework02"></p>
+	<p>Please input Language <input type="text" name="language02"></p>
+	<p>Please input Framework <input type="text" name="framework99"></p>
 	<p><input type="submit"></p>
 	</form>"""
 
 if __name__ == "__main__":
 	app.run(debug = True, port = 3000)
 
-# 現在來測試 .get() 和直接用 [] 的差異
-# 首先把 language02 改為 language99
-# 會發現，由於 表格輸入的資料 = language99 但是 request.form 想要 get 的資料是 language02
-# 所以 request.form.get("language02") 一直無法取得資料
-# 點下提交後永遠都會顯示 The language is None 無論是否有無在第一個表格放入資料
+# 把 framework02 改為 framework99
+# 會發現，由於 表格輸入的資料 = framework99 但是 request.form 想要的資料是 framework02
+# 所以 request.form["framework02"] 一直無法取得資料
+# 而且因為是使用 [] 格式，無論是否填入資料，提交後必定讓網頁報錯
+# werkzeug.exceptions.BadRequestKeyError
+# KeyError: 'framework02'
