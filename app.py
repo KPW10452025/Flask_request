6f07b77f3b2652179ce8e96dfa2dc9362ee8e415
PoWei <10452025@mail.ntou.edu.tw>
2021-08-02 00:06:50 +0900

    解決 Method Not Allowed 問題

--- app.py
+++ app.py
@@ -12,8 +12,11 @@ def query_example():
 	          <p>The framework is : {}<p>
 	          <p>The website is : {}<p>""".format(language01, framework01, website01)
 
-@app.route("/form_example")
+@app.route("/form_example", methods=["POST", "GET"])
 def form_example():
+	if request.method == "POST":
+		return "<p>Submitted Form</P>"
+
 	return """<form method="POST"> 
 	<p>Please input Language <input type="text" name="language"></p>
 	<p>Please input Framework <input type="text" name="framework"></p>
@@ -23,6 +26,7 @@ def form_example():
 if __name__ == "__main__":
 	app.run(debug = True, port = 3000)
 
-# 運用 form 製作表格
-# 在瀏覽輸入網址 localhost:3000/form_example 便能看到兩行填寫表格和一個提交資料的按鈕
-# 但目前只要點選提交後，頁面都會轉換到 Method Not Allowed
+# 目前只要點選提交後，頁面都會轉換到 Method Not Allowed
+# 在 route 中添加 methods=["POST", "GET"]
+# 並用 if 判斷 request.method 是否為 POST 是的話頁面顯示 Submitted Form
+# 重新開啟 localhost:3000/form_example 後，現在按下提交會就會出現已設定好的 Submitted Form
